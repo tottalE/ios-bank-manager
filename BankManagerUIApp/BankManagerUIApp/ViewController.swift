16041c1496b3f0d3beb1f7422bc2a874f830f10f
jin <pearlisrich@naver.com>
2022-11-11 15:23:30 +0900

    feat: customerAddButton, resetButton 추가 및 기초 autolayout

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -7,12 +7,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum Constant {
+        static let customerAddButtonText = "고객 10명 추가"
+        static let resetButtonText = "초기화"
+    }
+    
+    let customerAddButton: UIButton = {
+        let customerAddButton = UIButton()
+        customerAddButton.setTitle(Constant.customerAddButtonText, for: .normal)
+        customerAddButton.setTitleColor(.systemBlue, for: .normal)
+        customerAddButton.translatesAutoresizingMaskIntoConstraints = false
+        return customerAddButton
+    }()
+    
+    let resetButton: UIButton = {
+        let resetButton = UIButton()
+        resetButton.setTitle(Constant.resetButtonText, for: .normal)
+        resetButton.setTitleColor(.systemRed, for: .normal)
+        resetButton.translatesAutoresizingMaskIntoConstraints = false
+        return resetButton
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        view.addSubview(customerAddButton)
+        view.addSubview(resetButton)
+        setupButtonConstraint()
     }
 
+    func setupButtonConstraint() {
+        NSLayoutConstraint.activate([
+            customerAddButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            customerAddButton.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 30)
+        ])
+        
+        NSLayoutConstraint.activate([
+            resetButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            resetButton.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -30)
+        ])
+    }
 
 }
 
