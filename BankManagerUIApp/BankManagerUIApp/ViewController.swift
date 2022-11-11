c3915bf536b9da42876202dfc0bc9b9d321b4c8c
hashswim <ss991117@naver.com>
2022-11-11 17:01:16 +0900

    feat: 업무시간 Timer 구현

--- BankManagerUIApp/BankManagerUIApp/ViewController.swift
+++ BankManagerUIApp/BankManagerUIApp/ViewController.swift
@@ -8,12 +8,40 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var timer = Timer()
+    var (minutes, seconds, milliseconds) = (0, 0, 0)
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(self.keepTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func keepTimer() {
+        milliseconds += 1
+        seconds = (milliseconds/1000)%60
+        minutes = seconds/60
+        
+        let minuteStr = String(format:"%02d", minutes)
+        let secondStr = String(format:"%02d", seconds)
+        let millisecondsStr = String(format:"%03d", milliseconds%1000)
+        
+        timeLabel.text = "업무시간 - " + minuteStr + ":" + secondStr + ":" + millisecondsStr
+    }
+    
     enum Constant {
         static let customerAddButtonText = "고객 10명 추가"
         static let resetButtonText = "초기화"
         static let waitingLabelText = "대기중"
         static let processingLableText = "업무중"
     }
+    
+    var timeLabel: UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = .preferredFont(forTextStyle: .title2)
+        label.text = "00:00:000"
+        return label
+    }()
     
     let buttonStackView: UIStackView = {
         let buttonStackView = UIStackView()
@@ -75,20 +103,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupView()
+        startTimer()
         setupButtonConstraint()
+        setUpTimeConstraint()
         setupLineLabelConstraint()
     }
     
     func setupView() {
         view.addSubview(buttonStackView)
+        view.addSubview(timeLabel)
         view.addSubview(lineLabelStackView)
         buttonStackView.addArrangedSubview(customerAddButton)
         buttonStackView.addArrangedSubview(resetButton)
         lineLabelStackView.addArrangedSubview(waitingLabel)
         lineLabelStackView.addArrangedSubview(processingLabel)
-
     }
-
+    
     func setupButtonConstraint() {
         NSLayoutConstraint.activate([
             buttonStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -97,13 +127,20 @@ class ViewController: UIViewController {
         ])
     }
     
+    func setUpTimeConstraint() {
+        NSLayoutConstraint.activate([
+            timeLabel.topAnchor.constraint(equalTo: buttonStackView.bottomAnchor),
+            timeLabel.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            timeLabel.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
+        ])
+    }
+    
     func setupLineLabelConstraint() {
         NSLayoutConstraint.activate([
-            lineLabelStackView.topAnchor.constraint(equalTo: buttonStackView.bottomAnchor),
+            lineLabelStackView.topAnchor.constraint(equalTo: timeLabel.bottomAnchor),
             lineLabelStackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
             lineLabelStackView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
         ])
     }
-
 }
 
